65ce861da6246d3b0f134860f037e8b20213390a
RubenMkrtchyan30 <rubmkrtchyan2015@mail.ru>
2020-10-27 22:18:49 +0400

    27.10

--- 2/xo.py
+++ 2/xo.py
@@ -1,5 +1,39 @@
+all_steps = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+import random as r
+
 def tablo():
-	print(1, 2, 3)
-	print(4, 5, 6)
-	print(7, 8, 9)
-tablo()
+	print('')
+	print('')	
+	print('			',all_steps[0],'|', all_steps[1],'|', all_steps[2],'	')
+	print('			-----------',	 )
+	print('			',all_steps[3],'|', all_steps[4],'|', all_steps[5],'	')
+	print('			-----------',	 )
+	print('			',all_steps[6],'|', all_steps[7],'|', all_steps[8])
+	print('')
+	print('')
+
+
+minus = []
+
+def pc():
+	while True:
+		comp = r.randint(1,9)
+		if comp not in minus:
+			all_steps[comp-1] = 'O'
+			print(comp)
+			minus.append(comp)
+			break
+
+def user():
+	while True:
+		user = int(input('enter your number'))
+		all_steps[user-1] = 'X'
+		minus.append(user)
+		break
+
+while True:
+	tablo()
+	user()
+	tablo()
+	pc()
+	
