1683b372f644e616ccd32b96dbfb5934f632473d
RubenMkrtchyan30 <rubmkrtchyan2015@mail.ru>
2020-11-14 22:10:24 +0400

    a

---
+++ 2/3/exalgoritm.py
@@ -0,0 +1,33 @@
+# def mylist(a,b,c):
+# 	list_res = []
+# 	for i in range(a,b+1,c):
+# 		list_res.append(i)
+	
+# 	return list_res
+# print(mylist(10,100,20))
+
+# def new_list(n):
+# 	res = [3,7,12,5,20,0]
+# 	for i in range(len(n)-1):
+# 		print(n[i]*n[i+1])
+
+# new_list([3,7,5,6])
+
+# text = '_, we have a _.'
+
+# mylist = ['Houston','problem']
+
+# for i in text:
+# 	if i == '_':
+# 		text.replace(i,mylist[0])
+
+# print(text)
+
+# def sum_word(list1):
+# 	res = [] 
+# 	for i in list1:
+# 		res.append(len(i))
+# 	print(min(res)+max(res))
+# sum_word(['adasdsa','asd','asddas'])
+
+def 
