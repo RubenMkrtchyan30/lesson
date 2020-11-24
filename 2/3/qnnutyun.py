3d2a6946037cf5b7b87938d1538f2b6ecc594576
RubenMkrtchyan30 <rubmkrtchyan2015@mail.ru>
2020-11-24 22:12:43 +0400

    qnn

---
+++ 2/3/qnnutyun.py
@@ -0,0 +1,34 @@
+# class Coronavirus:
+# 	def __init__(self,celsius):
+# 		self.celsius = celsius
+
+# 	def temperature(self):
+# 		import math as m
+# 		farenheit = (self.celsius * m.pi) 
+# 		farenheit = m.ceil(farenheit)
+# 		if farenheit >  119 and farenheit < 128:
+# 			return 'You Have coronavirus', self.celsius
+# 		else:
+# 			return 'Everithyng is ok', self.celsius
+
+
+# cel = float(input('enter your temperature '))
+# res = Coronavirus(cel)
+# print(res.temperature())
+
+class Name:
+	def __init__(self,your_name):
+		self.your_name = your_name
+
+	def res(self):
+		result = 0
+		dict1 = {1 = 'a',' j', 's'  2 = 'b', 'k', 't';  3 =' c', 'l', 'u';  4 = 'd', 'm', 'v';   5 = 'e', 'n', 'w';   6 = 'f', 'o', 'x'; 
+7 = 'g', 'p', 'y';   8 = 'h', 'q', 'z'   9 = 'i','r'}
+		return d
+		for i in self.your_name:
+			if i in dict1[1.value]:
+				result += 1
+				return result
+your_name = input('enter your name ')
+result = Name(your_name)
+print(result.res())
